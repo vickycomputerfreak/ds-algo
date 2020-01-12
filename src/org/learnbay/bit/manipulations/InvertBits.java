178b775e77d7f903e7fa42895627c83fd3ccd46d
Vicky <Vicky@vickys-mbp>
2020-01-12 20:26:22 +0530

    INITIAL COMMIT

---
+++ src/org/learnbay/bit/manipulations/InvertBits.java
@@ -0,0 +1,18 @@
+package org.learnbay.bit.manipulations;
+
+public class InvertBits {
+	
+	static void invertBits(int num) {
+		
+		int x = (int) (Math.log(num)/Math.log(2)) + 1;
+		for(int i = 0; i < x; i++) 
+			num = (num ^ (1 << i));
+		System.out.println(num);
+		
+	}
+	
+	public static void main(String args[]) {
+		invertBits(10);
+		invertBits(11);
+	}
+}
