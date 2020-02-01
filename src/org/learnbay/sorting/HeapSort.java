1fb1a3f9ccd2a84f0ba7454e1fbd34c69d2f1314
Vicky <Vicky@vickys-mbp>
2020-02-01 12:03:47 +0530

    Heap Sort

--- src/org/learnbay/sorting/HeapSort.java
+++ src/org/learnbay/sorting/HeapSort.java
@@ -21,31 +21,33 @@ public class HeapSort {
 	
 	public void printArray(int arr[], String content) {
 		System.out.println(content);
-		for(int i = 0; i < arr.length - 1; i++)
+		for(int i = 0; i < arr.length; i++)
 			System.out.print(arr[i] + " ");
 		System.out.println();
 	}
 	
 	public void sort(int arr[]) {
 		int length = arr.length;
-		for(int i = length/2 - 1; i > 0; i --)
+		for(int i = length/2 - 1; i >= 0; i --)
 			heapify(arr, length, i);
-		for(int i = length -1; i > 0; i --) {
+		for(int i = length -1; i >= 0; i --) {
 			int temp = arr[0];
 			arr[0] = arr[i];
 			arr[i] = temp;
+			heapify(arr, i , 0);
 		}
 	}
 	
+
 	public void heapify(int arr[], int n , int i) {
 		int largest = i;
 		int l = 2 * i  + 1;
 		int r = 2 * i + 2;
 		if( l < n && arr[l] > arr[largest] )
 			largest = l;
-		if( r < n && arr[r] > arr[largest])
+		if( r < n && arr[r] > arr[largest] )
 			largest = r;
-		if(largest != i) {
+		if( largest != i ) {
 			int swap = arr[i];
 			arr[i] = arr[largest];
 			arr[largest] = swap;
